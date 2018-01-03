f3b8f2e4d275b8885ced8c341ed7564987cd3a5e
JFreud <jrm.frdnbrg@gmail.com>
2018-01-03 13:33:57 -0500

    added login fxn

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 from flask import Flask, render_template, request, session, redirect, url_for, flash
 from random import *
 import json, urllib2, sys
+from utils import db
 
 lat = 0
 long = 0
 
 my_app = Flask(__name__)
+app.secret_key = os.urandom(64)
+
 
 @my_app.route('/')
 def root():
@@ -13,6 +16,14 @@ def root():
     long = uniform(-180,180)
     print "latitude: ", lat, "longitude: ", long
     return render_template("game.html", latitude = lat, longitude = long, dictionary = status())
+
+@my_app.route('/login')
+def login():
+    if "user" in session:
+        return redirect(url_for('root'))
+    return render_template('login.html')
+
+
 
 def status():
     object = urllib2.urlopen("https://maps.googleapis.com/maps/api/geocode/json?latlng=" + str(lat) + "," + str(long) + "&key=AIzaSyCUg-iqhEm80I79bL5wCQ-_qB5bJxE76ro")
